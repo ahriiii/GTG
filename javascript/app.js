ac03c3690aab84b30bba7ec53a05cdd92441af8d
Sinon <47292896+ahriiii@users.noreply.github.com>
2024-09-07 16:33:23 +0800

    dONE

---
+++ javascript/app.js
@@ -0,0 +1,55 @@
+// Email Handling
+
+var sendMail = function () {
+    (function () {
+        emailjs.init("2BVln4tRECisJ02fq");
+    })();
+    var params = {
+        clientName: document.querySelector("#full-name").value,
+        phone: document.querySelector("#your-phone").value,
+        email: document.querySelector("#your-email").value,
+        subject: document.querySelector("#your-subject").value,
+        message: document.querySelector("#your-message").value,
+        toName: "GTG"
+    };
+
+    var serviceID = "service_xxfbtmp";
+    var templateID = "template_dhj0ltr";
+
+    emailjs.send(serviceID, templateID, params)
+        .then(res => {
+            swal("Message Sent", "Please wait for reply 😊", "success");
+        }).catch();
+}
+
+// btt button
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+    ) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+// AOS
+AOS.init({
+    duration: 1000
+});
